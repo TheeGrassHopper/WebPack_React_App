b495dbfcabd8e4c290b644a245defb11fdafc958
TheeGrassHopper <almortada88@gmail.com>
2016-05-28 15:58:22 -0500

    refactoring code to es6

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,7 +5,12 @@ var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
 var Firebase = require('firebase');
-var helpers = require('../utils/helpers');
+
+
+
+// var helpers = require('../utils/helpers');
+import getGithubInfo from '../utils/helpers'; // another way to write the line above
+
 
 var Profile = React.createClass({
   mixins: [ReactFireMixin],
@@ -31,7 +36,7 @@ var Profile = React.createClass({
  	var childRef = this.ref.child(username);
     	this.bindAsArray(childRef, 'notes');
 
-    helpers.getGithubInfo(username)
+    getGithubInfo(username)
       .then(function(data){
         this.setState({
           bio: data.bio,
